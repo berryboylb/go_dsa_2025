eb00713b2587f7e2bd1e75b428e0c16fef51572a
berryboylb <phemmynesce4life@gmail.com>
2024-12-31 20:47:44 +0100

    @chore: setup

---
+++ dsa_01_01_25/index.go
@@ -0,0 +1,9 @@
+package dsa_01_01_25
+
+import (
+	"fmt"
+)
+
+func Dsa() {
+	fmt.Println("Hello World from main_01_01_25")
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	dsa_01 "dsa/dsa_01_01_25"
+)
+
+func main() {
+	dsa_01.Dsa()
+}
