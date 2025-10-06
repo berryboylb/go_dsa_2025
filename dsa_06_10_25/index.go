d156e0f099aab815cc773886f8c480ac92cdf89e
berryboylb <phemmynesce4life@gmail.com>
2025-10-06 19:14:45 +0100

    @chore: starting afresh

--- dsa_06_10_25/index.go
+++ dsa_06_10_25/index.go
@@ -1,5 +1,7 @@
 package dsa_06_10_25
 
+
+
 func solution(nums []int, target int) []int {
 
 	items := map[int]int{}
@@ -16,6 +18,24 @@ func solution(nums []int, target int) []int {
 	return []int{}
 
 }
+
+func bruteForce(nums []int, target int) []int {
+
+	for i, v := range nums {
+
+		remainder := target - v
+
+		for index, innerValue := range nums {
+			if innerValue == remainder && i != index {
+				return []int{i, index}
+			}
+
+		}
+
+	}
+	return []int{}
+}
+
 
 func Main() {
 	solution([]int{2, 7, 11, 15}, 9)
