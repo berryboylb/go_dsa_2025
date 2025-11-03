1d110744a119818ac1672c1a77c84cd6c68e5a29
berryboylb <phemmynesce4life@gmail.com>
2025-11-03 23:10:53 +0100

    dsa 03

---
+++ dsa_03_11_25/index.go
@@ -0,0 +1,23 @@
+package dsa_03_11_25
+
+
+
+// https://leetcode.com/problems/house-robber/description/
+
+
+
+func solution(nums []int) int {
+	rob1, rob2, maxRes := 0, 0, 0
+
+	for i := 0; i < len(nums); i++ {
+		maxRes = max(rob1+nums[i], rob2)
+		rob1 = rob2
+		rob2 = maxRes
+	}
+
+	return maxRes
+}
+
+func Main() {
+	solution([]int{1, 2, 3, 1})
+}
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	dsa "dsa/dsa_29_10_25"
+	dsa "dsa/dsa_03_11_25"
 )
 
 func main() {
